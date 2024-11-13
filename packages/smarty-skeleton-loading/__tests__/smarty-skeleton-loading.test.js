1ceea282c0043b82f7047e323809905a833dfd95
huabuyu05100510 <huabuyu0510@163.com>
2024-11-13 22:08:17 +0800

    loading

--- packages/smarty-skeleton-loading/__tests__/smarty-skeleton-loading.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const smartySkeletonLoading = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(smartySkeletonLoading(), 'Hello from smartySkeletonLoading');
-console.info('smartySkeletonLoading tests passed');
--- packages/smarty-skeleton-loading/lib/smarty-skeleton-loading.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = smartySkeletonLoading;
-
-function smartySkeletonLoading() {
-  return 'Hello from smartySkeletonLoading';
-}
