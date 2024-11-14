dbe61e178b6e88816563fe70a2f8a6c3fb7ca4cd
huabuyu05100510 <huabuyu0510@163.com>
2024-11-14 19:49:45 +0800

    init

---
+++ packages/smarty-skeleton-docs/.umi/dumi/layout.tsx
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import React from 'react';
+import config from '@@/dumi/config';
+import demos from '@@/dumi/demos';
+import apis from '@@/dumi/apis';
+import Layout from '/Users/huabuyu/Documents/smarty-skeleton/node_modules/.pnpm/@umijs+preset-dumi@1.1.48_react-dom@16.14.0_react-router@6.28.0_react@17.0.2_typescript@5.6.3_umi@3.5.43/node_modules/@umijs/preset-dumi/lib/theme/layout';
+
+export default (props) => <Layout {...props} config={config} demos={demos} apis={apis} />;
